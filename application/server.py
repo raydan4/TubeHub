f3e2c7ab53f3ef0ac541ae34dff56ce0f9351f44
Logan Murdock <lxm8708@rit.edu>
2019-11-22 10:11:46 -0500

    implemented adding user to database

--- application/server.py
+++ application/server.py
@@ -1,17 +1,32 @@
 import urllib.request 
-
+import pymysql
+from pymysql import cursors
+from datetime import datetime
 from flask import (
         Blueprint,
         Flask,
         render_template,
-        request
+        request,
+        redirect,
+        flash
 )
+#from flask_mysqldb import MySQL
+#pymysql
+
 from blueprints.searchAPI.search import search
 from blueprints.accountAPI.account import account
 from blueprints.videoAPI.video import video
 
 app = Flask(__name__)
 
+connection = pymysql.connect(host='maria',
+                             user='root',
+                             password='changeme',
+                             db='tubehub',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+cur = connection.cursor()
 
 @app.route('/')
 def index():
@@ -22,9 +37,21 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        u = User(username, password)
-        db.session.add(u)
-        db.session.commit()
+        date = datetime.now()
+
+
+        sql = "SELECT `username` FROM `users` WHERE `username` = %s;"
+        cur.execute(sql, (username))
+    
+        if cur.fetchone() == username:
+            #flash("Username already taken.")
+            return redirect("/")
+        
+        
+        sql = "INSERT INTO `users` (`username`, `password`, `birthdate`, `email`, `sessionid`) VALUES (%s, %s, %s, %s, %s)"
+        cur.execute(sql, (username, password, date, "example@email.com", "jkfal13k3jfdq"))
+        connection.commit()
+
         return redirect("/login")
     if request.method == 'GET':
         return render_template("signup.html")
@@ -35,9 +62,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        u = User(username, password)
-        db.session.add(u)
-        db.session.commit()
+        
+
+
         return redirect("/login")
     if request.method == 'GET':
         return render_template("signin.html")
