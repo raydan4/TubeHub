8647ef737a23d8caecfdb873118a3ae515f1c039
raydan4 <dszafran116@gmail.com>
2019-11-23 16:29:16 -0500

    -_-

--- test/login_test.py
+++ test/login_test.py
@@ -2,31 +2,17 @@ import pytest
 from requests import post
 
 def login(data):
-    try:
-        r = post("https://localhost/login", data=data, verify=False)
-    except Exception as e:
-        print(e)
-        return None
-    return r.ok
+    return post("http://localhost/login", data=data).ok
 
-def goodlogin_test(data):
-    assert login(data)
+def test_goodlogin():
+    assert login({"username": "pytest", "password":"testpassword"})
 
-def badpass_test(data):
-    assert login(data)
+def test_badpass():
+    assert not login({"username": "pytest", "password":"badpass"})
 
-def baduser_test(data):
-    assert login(data) 
+def test_baduser():
+    assert not login({"username": "badusername", "password":"testpassword"}) 
 
-def badeverything_test(data):
-    assert login(data)
-
-
-goodlogin_test({"username": "pytest", "password":"testpassword"})
-
-badpass_test({"username": "pytest", "password":"badpass"})
-
-baduser_test({"username": "badusername", "password":"testpassword"})
-
-baseverything_test({"username": "badusername", "password":"badpass"})
+def test_badeverything():
+    assert not login({"username": "badusername", "password":"badpass"})
 
